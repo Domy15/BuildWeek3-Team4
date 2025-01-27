ebf51304b9ee5541bdc12c0b7df60e39f6523674
LookThisHook <lucagiordano634@gmail.com>
2025-01-27 14:51:05 +0100

    footer da controllare

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './App.css'
+import FooterSection from "./components/footer";
 
 function App() {
 
   return (
     <>
+    <FooterSection/>
       </>
   )
 }
