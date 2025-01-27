44cfad038075f8ec2507d31686c13ba6aa2f7edb
Ninfa-C <ninfa_95@hotmail.it>
2025-01-27 11:26:41 +0100

    ts jobs, comments e posts

---
+++ src/types/Jobs.ts
@@ -0,0 +1,14 @@
+export interface Job {
+    role:        string;
+    company:     string;
+    startDate:   Date;
+    endDate:     Date;
+    description: string;
+    area:        string;
+    username:    string;
+    image:       string;
+    createdAt:   string;
+    updatedAt:   string;
+    __v:         number;
+    _id:         string;
+}
---
+++ src/types/Post.ts
@@ -0,0 +1,8 @@
+export interface Post {
+    text:      string;
+    username:  string;
+    createdAt: string;
+    updatedAt: string;
+    __v:       number;
+    _id:       string;
+}
